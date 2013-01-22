4410e03df12b99c46467b6fe93f7b8cb206d441c
Eliot <eliot@tapir>
2013-01-22 11:18:09 -0800

    Remove unreachable code. Use functools.wraps.
     - Remove code that was unreachable. Thanks Jaskirat
       (http://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/#c24691)
     - Use functools.wraps to make the retry decorator "well behaved"
     - Fix typo

--- decorators.py
+++ decorators.py
@@ -1,4 +1,6 @@
 import time
+from functools import wraps
+
 
 def retry(ExceptionToCheck, tries=4, delay=3, backoff=2, logger=None):
     """Retry calling the decorated function using an exponential backoff.
@@ -7,7 +9,7 @@ def retry(ExceptionToCheck, tries=4, delay=3, backoff=2, logger=None):
     original from: http://wiki.python.org/moin/PythonDecoratorLibrary#Retry
 
     :param ExceptionToCheck: the exception to check. may be a tuple of
-        excpetions to check
+        exceptions to check
     :type ExceptionToCheck: Exception or tuple
     :param tries: number of times to try (not retry) before giving up
     :type tries: int
@@ -20,14 +22,13 @@ def retry(ExceptionToCheck, tries=4, delay=3, backoff=2, logger=None):
     :type logger: logging.Logger instance
     """
     def deco_retry(f):
+
+        @wraps(f)
         def f_retry(*args, **kwargs):
             mtries, mdelay = tries, delay
-            try_one_last_time = True
             while mtries > 1:
                 try:
                     return f(*args, **kwargs)
-                    try_one_last_time = False
-                    break
                 except ExceptionToCheck, e:
                     msg = "%s, Retrying in %d seconds..." % (str(e), mdelay)
                     if logger:
@@ -37,8 +38,8 @@ def retry(ExceptionToCheck, tries=4, delay=3, backoff=2, logger=None):
                     time.sleep(mdelay)
                     mtries -= 1
                     mdelay *= backoff
-            if try_one_last_time:
-                return f(*args, **kwargs)
-            return
+            return f(*args, **kwargs)
+
         return f_retry  # true decorator
+
     return deco_retry
