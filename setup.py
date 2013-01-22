7f044ed6f583af42179c6548428ce69c9049bb3c
Eliot <eliot@tapir>
2013-01-22 11:13:25 -0800

    Add setup.py; update .gitignore

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="retry-decorator",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Eliot",
+    author_email="saltycrane@gmail.com",
+)
